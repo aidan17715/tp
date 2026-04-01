9d89611e01ef0b71b43ad8b8e23cdc682b483057
aidan17715 <aidan.chia.tong@gmail.com>
2026-04-01 23:10:29 +0800

    test: add expiry date tests to InventoryTest
    - Test adding ingredients with expiry dates
    - Test adding ingredients without expiry dates
    - Test storing multiple ingredients with different expiry dates
    - Verify expiry dates are correctly retrieved
    
    All 10 tests passing.

--- src/test/java/seedu/sudocook/InventoryTest.java
+++ src/test/java/seedu/sudocook/InventoryTest.java
@@ -3,6 +3,9 @@ package seedu.sudocook;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.time.LocalDate;
 
 public class InventoryTest {
     private Inventory testInventory = new Inventory();
@@ -40,5 +43,32 @@ public class InventoryTest {
         Command cmd = parser.parse("add-i Gibberish");
         cmd.execute(testInventory);
         assertEquals(0, testInventory.size());
+    }
+
+    // --- Expiry Tests ---
+    @Test
+    public void addIngredient_withExpiry_storesExpiryDate() {
+        LocalDate expiry = LocalDate.of(2026, 12, 31);
+        testInventory.addIngredient(new Ingredient("Milk", 2, "L", expiry));
+        assertEquals(1, testInventory.size());
+        assertEquals(expiry, testInventory.getIngredient(0).getExpiryDate());
+    }
+
+    @Test
+    public void addIngredient_withoutExpiry_expiryIsNull() {
+        testInventory.addIngredient(new Ingredient("Sugar", 100, "g"));
+        assertEquals(1, testInventory.size());
+        assertNull(testInventory.getIngredient(0).getExpiryDate());
+    }
+
+    @Test
+    public void addIngredient_twoIngredientsWithExpiry_bothStored() {
+        LocalDate expiry1 = LocalDate.of(2026, 6, 1);
+        LocalDate expiry2 = LocalDate.of(2026, 12, 31);
+        testInventory.addIngredient(new Ingredient("Milk", 1, "L", expiry1));
+        testInventory.addIngredient(new Ingredient("Butter", 100, "g", expiry2));
+        assertEquals(2, testInventory.size());
+        assertEquals(expiry1, testInventory.getIngredient(0).getExpiryDate());
+        assertEquals(expiry2, testInventory.getIngredient(1).getExpiryDate());
     }
 }
